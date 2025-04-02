f617d027da508d63ff461baf410b54f2d797cd1f
Infernocasis <0990106121sA@mail.ru>
2025-04-02 15:51:44 +0300

    Я красава

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module X
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+ 
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
